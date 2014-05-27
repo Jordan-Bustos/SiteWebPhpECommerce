669ae81a958fba2dfbe3fa28f22f7c6dc79a172b
Jordan Bustos <jordan.bustos.pro@gmail.com>
2014-05-27 08:43:19 +0200

    Do work connexion

--- public_html/classes/DataBaseManager_class.php
+++ public_html/classes/DataBaseManager_class.php
@@ -21,7 +21,7 @@
 			try
 			{
 				self::$lienBDD = mysqli_init();
-				mysqli_real_connect(self::$lienBDD, "localhost", "root", "", "dbjepoinas")
+				mysqli_real_connect(self::$lienBDD, "localhost", "root", "root", "dbjepoinas")
 				or die ("Impossible de se connecter &agrave; mysql (base de donn&eacute;es bdd) :
 				".mysqli_error(self::$lienBDD));
 			}
